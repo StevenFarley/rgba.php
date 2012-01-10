58c05569e5fda2ccd55ea5b48370f081d2da743e
Steven Farley <steven@stevenfarley.me>
2012-01-10 16:16:03 -0500

    Reformatted and modified code structure

--- rgba.php
+++ rgba.php
@@ -10,13 +10,12 @@
 ######## SETTINGS ##############################################################
 
 /**
- * Enter the directory in which to store cached color images.
+ * Enter the directory in which to store cached PNG images.
  * This should be relative and SHOULD contain a trailing slash.
  * The directory you specify should exist and be writeable (usually chmoded to 777).
  * If you want to store the pngs at the same directory, leave blank ('').
  */
-define('COLORDIR', 'colors/');
-
+define('PNGS_DIR', ''); //Defaulted to same directory this script is in
 
 /**
  * If you frequently use a color with varying alphas, you can name it
@@ -49,13 +48,13 @@ $color_names = array(
  * large), some people argue that the browser needs to spend substantially more resources
  * to render the background when the image is too small (like 1x1).
  */
-define('SIZE', 10);
+define('PNG_SIZE', 10);
 
 /**
  * If you don't want the generated pngs to be cached on the server, set the following to
  * false. This is STRONGLY NOT RECOMMENDED. It's here only for testing/debugging purposes.
  */
-define('CACHEPNGS', true);
+define('CACHE_PNGS', TRUE);
 
 ######## NO FURTHER EDITING, UNLESS YOU REALLY KNOW WHAT YOU'RE DOING ##########
 
@@ -63,41 +62,53 @@ define('CACHEPNGS', true);
 // extra byte will prevent it from showing up
 error_reporting(E_ERROR | E_PARSE);
 
-$dir = substr(COLORDIR, 0, strlen(COLORDIR) - 1);
+$dir = substr(PNGS_DIR, 0, strlen(PNGS_DIR) - 1);
 
-if (!is_writable($dir)) {
-	die("The directory '$dir' either doesn't exist or isn't writable.");
+if (! is_writable($dir))
+{
+	die("The directory '{$dir}' either doesn't exist or isn't writable.");
 }
 
 // Are the RGB values provided directly or implied through a named color?
-if (isset($color_names[$_REQUEST['name']])) {
+if (isset($color_names[$_REQUEST['name']]))
+{
 	list($red, $green, $blue) = $color_names[$_REQUEST['name']];
 	$alpha = $_REQUEST['a'] / 100;
 }
-else {
-	if(isset($_REQUEST['r']) and isset($_REQUEST['g']) and isset($_REQUEST['b'])) {
+else
+{
+	if (isset($_REQUEST['r']) && isset($_REQUEST['g']) && isset($_REQUEST['b']))
+	{
 		// Old way: rgba.php?r=R&g=G&b=B&a=100*A
 		$color_info = $_REQUEST;
 		$alpha = $_REQUEST['a'] / 100;
 	}
-	else {
+	else
+	{
 		$color_array = explode(',', $_SERVER['PATH_INFO']);
-		if (count($color_array) == 2) {
+
+		if (count($color_array) === 2)
+		{
 			// New way for names: rgba.php/colorname,alpha
-			if (isset($color_names[$color_array[0]])) {
+			if (isset($color_names[$color_array[0]])) 
+			{
 				$color_info = $color_names[$color_array[0]];
 				$color_info[] = floatval($color_array[1]);
-			} else {
+			}
+			else
+			{
 				die("Color name {$color_array[0]} unknown.");
 			}
-		} else {
+		}
+		else 
+		{
 			// New way: rgba.php/rgba(R,G,B,A)
 			$color_info = explode(',', str_replace(' ', '', substr($_SERVER['PATH_INFO'], 6, -1)));
 			$color_info = array_combine(array('r','g','b','a'), $color_info);
 			$alpha	= floatval($color_info['a']);
 		}
 	}
-	
+
 	$red	= intval($color_info['r']);
 	$green	= intval($color_info['g']);
 	$blue	= intval($color_info['b']);
@@ -113,36 +124,39 @@ header('Expires: 01 Jan '.(date('Y') + 10).' 00:00:00 GMT');
 header('Cache-control: max-age=2903040000');
 
 // Does it already exist?
-$filepath = COLORDIR . "color_r{$red}_g{$green}_b{$blue}_a{$alpha}.png";
+$filepath = PNGS_DIR . "color_r{$red}_g{$green}_b{$blue}_a{$alpha}.png";
 
-if(CACHEPNGS and file_exists($filepath)) {
-
+if (CACHE_PNGS && file_exists($filepath))
+{
 	// The file exists, is it cached by the browser?
-	if (function_exists('apache_request_headers')) {
+	if (function_exists('apache_request_headers'))
+	{
 		$headers = apache_request_headers();
 
 		// We don't need to check if it was actually modified since then as it never changes.
-		$responsecode = isset($headers['If-Modified-Since'])? 304 : 200;
+		$responsecode = isset($headers['If-Modified-Since']) ? 304 : 200;
 	}
-	else {
+	else
+	{
 		$responsecode = 200;
 	}
 
-	header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($filepath)) . ' GMT', true, $responsecode);
+	header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($filepath)) . ' GMT', TRUE, $responsecode);
 
-	if ($responsecode == 200) {
-		header('Content-Length: '.filesize($filepath));
+	if ($responsecode === 200)
+	{
+		header('Content-Length: ' . filesize($filepath));
 		die(file_get_contents($filepath));
 	}
 }
-else {
-	$img = @imagecreatetruecolor(SIZE, SIZE)
-		  or die('Cannot Initialize new GD image stream');
+else
+{
+	$img = @imagecreatetruecolor(PNG_SIZE, PNG_SIZE) or die('Cannot Initialize new GD image stream');
 
 	// This is to allow the final image to have actual transparency
 	// http://www.php.net/manual/en/function.imagesavealpha.php
-	imagealphablending($img, false);
-	imagesavealpha($img, true);
+	imagealphablending($img, FALSE);
+	imagesavealpha($img, TRUE);
 
 	// Allocate our requested color
 	$color = imagecolorallocatealpha($img, $red, $green, $blue, $alpha);
@@ -151,12 +165,15 @@ else {
 	imagefill($img, 0, 0, $color);
 
 	// Save the file (if caching is allowed)
-	if (CACHEPNGS) {
+	if (CACHE_PNGS)
+	{
 		// Check PHP version to solve a bug that caused the script to fail on PHP versions < 5.1.7
-		if (strnatcmp(phpversion(), '5.1.7') >= 0) {
+		if (strnatcmp(phpversion(), '5.1.7') >= 0)
+		{
 			imagepng($img, $filepath, 0, NULL);
 		}
-		else {
+		else
+		{
 			imagepng($img, $filepath);
 		}
 	}
